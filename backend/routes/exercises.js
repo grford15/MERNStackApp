5ec3392474b78784747f6d180fd6fceb35912e8a
Greg <g.rutherford10@gmail.com>
2020-01-13 16:56:35 +0000

    fixed typo error in exercises route get method

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -3,7 +3,7 @@ let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
   Exercise.find()
-    .then(exercises => req.json(exercises))
+    .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json('Error' + err));
 });
 
