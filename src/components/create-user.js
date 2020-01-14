5e16bfad8fd3b8deb1ed7c69b4fef3b56a21f4d9
Greg <g.rutherford10@gmail.com>
2020-01-14 12:20:20 +0000

    add basic functionality to create a new user

--- src/components/create-user.js
+++ src/components/create-user.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
 
 export default class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+    };
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+    };
+
+    console.log(user);
+
+    this.setState({
+      username: '',
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <p>You are on the Create User component</p>
+        <h3>Create a new User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              className="btn btn-primary"
+              onSubmit={this.onSubmit}
+            />
+          </div>
+        </form>
       </div>
     );
   }
