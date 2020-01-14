2f3332e901b7fb4abb715d5050bef7332ef1aa4a
Greg <g.rutherford10@gmail.com>
2020-01-14 13:48:05 +0000

    updated functionality to create an exercise in the database

--- src/components/create-exercise.js
+++ src/components/create-exercise.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Datepicker from 'react-datepicker';
+import axios from 'axios';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export default class CreateExercise extends Component {
@@ -22,9 +23,13 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      users: ['test user'],
-      username: 'test user',
+    axios.get('http://localhost:5000/users/').then(response => {
+      if (response.data.length > 0) {
+        this.setState({
+          users: response.data.map(user => user.username),
+          username: response.data[0].username,
+        });
+      }
     });
   }
 
@@ -63,6 +68,10 @@ export default class CreateExercise extends Component {
     };
 
     console.log(exercise);
+
+    axios
+      .post('http://localhost:5000/exercises/add', exercise)
+      .then(res => console.log(res.data));
 
     window.location = '/';
   }
